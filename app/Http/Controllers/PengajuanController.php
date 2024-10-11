269e73b1044a5a1260b661f9b27eef6f42f7dcf2
fathanpr <2010631170072@student.unsika.ac.id>
2024-10-11 07:58:12 +0700

    fix update pengajuan

--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -6,6 +6,7 @@ use Throwable;
 use App\Models\Rekening;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class PengajuanController extends Controller
